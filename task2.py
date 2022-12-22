3662790d40fb3ff3ec43b6d3b9d260635e657ccf
Gajjar Mohit <114332766+Gajjar-Mohit@users.noreply.github.com>
2022-12-22 08:32:10 +0000

    complete3d task2

---
+++ task2.py
@@ -0,0 +1,21 @@
+# Create a list of  21,22,23,24,25
+list1 = [21, 22, 23, 24, 25]
+
+# Create a list of 22,24,26,28,30
+list2 = [22, 24, 26, 28, 30]
+
+# Join both list
+list3 = list1 + list2
+print(list3)
+
+# Count number of 24 in joined list
+print(list3.count(24))
+
+# Find index of 24
+print(list3.index(24))
+
+# Reverse the joined list
+print(list3.reverse())
+
+# Sorting the joined list
+print(list3.sort())
